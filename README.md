de42c7fe5e3be192d777e8a83296ac76b54d5870
Aaron Gentle <admin@classynutgaming.xyz>
2015-10-06 22:31:20 +1100

    Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ClassyNutGaming-Forums
-Discourse Theme
---
+++ plugin.rb
@@ -0,0 +1,4 @@
+# name: classynutgaming-layout
+# about: Custom layout developed for the ClassyNutGaming Forums
+# version: 0.0.1
+# authors: Aaron Gentle
