9a962c15938fc0472f93d2f6d1ab63faa80658f5
Aaron Gentle <admin@classynutgaming.xyz>
2015-10-07 16:41:20 +1100

    Moved category description into same parent as category title

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
 # name: classynutgaming-layout
-# about: Custom layout developed for the ClassyNutGaming Forums
+# about: Custom Discourse layout developed for the ClassyNutGaming Forums
 # version: 0.0.1
 # authors: Aaron Gentle
