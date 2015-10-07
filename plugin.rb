a6a75f18a0fa0b7a8a099360d25d2297a14292dc
Aaron Gentle <admin@classynutgaming.xyz>
2015-10-07 17:09:45 +1100

    register categories change

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,5 @@
 # about: Custom Discourse layout developed for the ClassyNutGaming Forums
 # version: 0.0.2
 # authors: Aaron Gentle
+
+register_asset "javascripts/discourse/templates/discovery/categories.hbs"
