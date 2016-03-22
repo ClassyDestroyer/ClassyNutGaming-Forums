5ccdd252efe505263f0f3247d228923919467604
Aaron Gentle <admin@classynutgaming.xyz>
2016-03-22 22:33:16 +1100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: classynutgaming-layout
 # about: Custom Discourse layout developed for the ClassyNutGaming Forums
-# version: 0.0.5
+# version: 0.0.6
 # authors: Aaron Gentle
 
 register_asset "javascripts/discourse/templates/discovery/categories.hbs"
